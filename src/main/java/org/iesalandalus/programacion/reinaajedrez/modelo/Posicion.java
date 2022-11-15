7370ac03a8a15e5de139ecc0e492660b757dbc2e
Sergio Fernández Pardo <96870789+Serferman@users.noreply.github.com>
2022-11-15 11:57:42 +0100

    Creacion de los metodos set y get para los atributos "fila" y "columna" en la clase "Posicion".

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -4,5 +4,28 @@ public class Posicion {
 
 	private int fila;
 	private char columna;
+
+	public int getFila() {
+		return fila;
+	}
+
+	private void setFila(int fila) {
+		if ((8 < fila) || (fila < 1)) {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		}
+		this.fila = fila;
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	private void setColumna(char columna) {
+		if (('a' > columna) || (columna > 'h')) {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		}
+		this.columna = columna;
+	}
+
 	
 }
