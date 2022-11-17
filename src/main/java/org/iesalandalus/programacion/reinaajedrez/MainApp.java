3359e95db0cc6d38892dd77ebc76f9deb8d2e9aa
Sergio Fernández Pardo <96870789+Serferman@users.noreply.github.com>
2022-11-17 10:44:48 +0100

    Creación del método "ejecutarOpcion" en la clase MainApp (Aun no esta terminado, porque depende de la creacion de los siguientes metodos).

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -9,4 +9,21 @@ public class MainApp {
 	public static void main(String[] args) {
 		
 	}
+	
+	private static void ejecutarOpcion(int opcion) {
+		switch (opcion) {
+		case 1:
+			break;
+
+		case 2:
+			break;
+
+		case 3:
+			break;
+
+		case 4:
+			break;
+		}
+	}
+	
 }
