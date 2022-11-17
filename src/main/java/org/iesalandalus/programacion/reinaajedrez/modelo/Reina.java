65ce3c479d58ba56ce6a8ee92c422746ead35960
Sergio Fernández Pardo <96870789+Serferman@users.noreply.github.com>
2022-11-17 10:02:59 +0100

    Creacion de los metodos set y get en la clase reina. El metodo "setColor" y "setPosicion" tiene las validaciones necesarias.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -5,4 +5,36 @@ public class Reina {
 	private Color color;
 	private Posicion posicion;
 
+	public Color getColor() {
+		return color;
+	}
+
+	private void setColor(Color color) {
+		if (color == null) {
+			throw new NullPointerException("ERROR: El color no puede ser nulo.");
+		}
+
+		if (Color.BLANCO == color) {
+
+			this.color = Color.BLANCO;
+			this.posicion = new Posicion(1, 'd');
+
+		} else if (Color.NEGRO == color) {
+
+			this.color = Color.NEGRO;
+			this.posicion = new Posicion(8, 'd');
+		}
+	}
+	
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setPosicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new NullPointerException("ERROR: La posicion no puede ser nula.");
+		}
+		this.posicion = posicion;
+	}
+	
 }
