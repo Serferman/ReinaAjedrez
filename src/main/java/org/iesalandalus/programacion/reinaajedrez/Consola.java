0bd29da9f39de12e44b85c9b1bf133f95b573128
Sergio Fernández Pardo <96870789+Serferman@users.noreply.github.com>
2022-11-17 10:29:31 +0100

    Creación del metodo "elegirOpcionMenu" en la clase de utilidades "Consola".

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
 
 	private Consola() {
@@ -14,6 +16,15 @@ public class Consola {
 		System.out.println("");
 		System.out.println("4.- Salir.");
 		System.out.println("--------------------------------");
+	}
+	
+	public static int elegirOpcionMenu() {
+		int opcion;
+		do {
+			System.out.println("Introduceme una opción de este menú: ");
+			opcion = Entrada.entero();
+		} while ((opcion < 1) || (opcion > 4));
+		return opcion;
 	}
 	
 }
