7dd2c2d066fb5d72f61034cebe17d81ad3882eda
Sergio Fernández Pardo <96870789+Serferman@users.noreply.github.com>
2022-11-17 10:10:47 +0100

    Creacion del metodo mover con las validaciones necesarias.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import javax.naming.OperationNotSupportedException;	
+
 public class Reina {
 
 	private Color color;
@@ -44,6 +46,57 @@ public class Reina {
 			throw new NullPointerException("ERROR: La posicion no puede ser nula.");
 		}
 		this.posicion = posicion;
+	}
+	
+	public void mover(Direccion direccion, int pasos) throws OperationNotSupportedException {
+
+		if (direccion == null) {
+			throw new NullPointerException("ERROR: La dirección no puede ser nula.");
+		}
+
+		if ((pasos < 1) || (pasos > 7)) {
+			throw new IllegalArgumentException("ERROR: El número de pasos debe estar comprendido entre 1 y 7.");
+		}
+
+		try {
+			switch (direccion) {
+			case NORTE:	
+				setPosicion(new Posicion((posicion.getFila() + pasos), posicion.getColumna()));
+				break;
+
+			case ESTE:
+				setPosicion(new Posicion(posicion.getFila(), (char)(posicion.getColumna() + pasos)));
+				break;
+
+			case SUR:
+				setPosicion(new Posicion((posicion.getFila() - pasos), posicion.getColumna()));
+				break;
+
+			case OESTE:
+				setPosicion(new Posicion(posicion.getFila(), (char)(posicion.getColumna() - pasos)));
+				break;
+
+			case NORESTE:
+				setPosicion(new Posicion((posicion.getFila() + pasos), (char)(posicion.getColumna() + pasos)));
+				break;
+
+			case SURESTE:
+				setPosicion(new Posicion((posicion.getFila() - pasos), (char)(posicion.getColumna() + pasos)));
+				break;
+
+			case SUROESTE:
+				setPosicion(new Posicion((posicion.getFila() - pasos), (char)(posicion.getColumna() - pasos)));
+				break;
+
+			case NOROESTE:
+				setPosicion(new Posicion((posicion.getFila() + pasos), (char)(posicion.getColumna() - pasos)));
+				break;
+
+			}
+		} catch (IllegalArgumentException e) {
+			throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+		}
+
 	}
 	
 }
